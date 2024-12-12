907f996991e11f671d71ab48cdf7194e9bd4e13c
agomez <a@a>
2024-12-12 18:59:19 +0100

    Refactor: cleanup entry point

--- XML_Checker.py
+++ XML_Checker.py
@@ -1,6 +1,6 @@
-# XML_Checker.py
-from src import xml_validator
+# Import the run_gui function from the gui module
+from src.gui import run_gui
 
-# Run the application
+# Main entry point to run the GUI application
 if __name__ == "__main__":
-    xml_validator.run_gui()
+    run_gui()
